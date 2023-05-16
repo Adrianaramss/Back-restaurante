ac7a803b301e594fbf9903a5f66416b9d3c6d840
Adriana Ramos <111310311+Adrianaramss@users.noreply.github.com>
2023-05-16 14:11:05 -0300

    Update knex.ts

--- src/database/knex.ts
+++ src/database/knex.ts
@@ -3,7 +3,7 @@ import knex from 'knex'
 export const db = knex({
     client: "sqlite3",
     connection: {
-        filename: "./src/database/labook.db", //localização do seu arquivo .db
+        filename: "./src/database/sistema.db", //localização do seu arquivo .db
     },
     useNullAsDefault: true, // definirá NULL quando encontrar valores undefined
     pool: {
